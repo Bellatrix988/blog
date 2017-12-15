2a711cc0a41ad9ddea09284c1fc3626579c292cd
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-12-15 16:45:38 +0300

    Add destroy of commentaris and simple auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "orange",
+                               password: "secret",
+                               except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -11,17 +16,39 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
-    if  @article.save
+    if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
 
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
